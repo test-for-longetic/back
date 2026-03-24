af20a74993fe6a23fb8c0a0e63bde58f9f4002cb
Vitalii Storcheus <vitalii.storcheus@preply.com>
2026-03-24 20:51:48 +0200

    update to deploy

--- src/server.ts
+++ src/server.ts
@@ -9,8 +9,7 @@ import { parseCsv } from "./utils/parseCsv";
 import { parsePdfText } from "./utils/parsePdfText";
 
 const app = express();
-const PORT = 4000;
-
+const PORT = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
